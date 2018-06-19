9045f53b428bfef5a60b90639a1847958d5e71db
kikinbmk10 <kikinbmk10@gmail.com>
2018-06-19 15:25:19 -0500

    [ADD] Primera vista

---
+++ fyf/resources/views/contacto.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Contacto | Pasta y Pasto</title>
+
+  <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
+  <link rel="icon" type="image/png" href="{{asset('img/contacto/icon.png')}}" sizes="128x128">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}"/>
+</head>
+
+<body>
+  <div class="contacto-franquisia">
+    <div class="contacto-header">
+      <img src="{{asset('img/contacto/Page1.png')}}" class="img-bg-beige">
+      <img src="{{asset('img/contacto/LogoBlanco.png')}}" class="img-logo-beige">
+      <img src="{{asset('img/contacto/Tenedor.png')}}" class="img-tenedor-beige">
+      <img src="{{asset('img/contacto/jitomateBeige.png')}}" class="img-jitomate1-beige">
+      <img src="{{asset('img/contacto/jitomateBeige2.png')}}" class="img-jitomate2-beige">
+      <img src="{{asset('img/contacto/PenneBeige.png')}}" class="img-penne-beige">
+      <img src="{{asset('img/contacto/SignSlogan.png')}}" class="img-slogan-beige">
+      <img src="{{asset('img/contacto/CamaronBeige.png')}}" class="img-camaron-beige">
+    </div>
+  </div>
+
+
+</body>
+</html>
--- fyf/routes/web.php
+++ fyf/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('contacto');
 });
 
 Route::get('/contacto', function(){
